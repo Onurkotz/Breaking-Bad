d183620eec86116a014f70f500855a0b9b85aca3
onur koc <onurkottz@gmail.com>
2022-04-05 17:47:22 +0300

    loading and error

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,31 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCharacters } from "../../redux/charactersSlice/charactersSlice";
+//import Loading from "../Loading/Loading";
+//import Error from "../Error/Error";
 
 function Home() {
-  const data = useSelector((state) => state.characters);
+  const data = useSelector((state) => state.characters.items);
+  //const isLoading = useSelector((state) => state.characters.isLoading);
+ // const error = useSelector((state) => state.characters.error);
   const dispatch = useDispatch();
+  
+  
   useEffect(() => {
     dispatch(fetchCharacters());
   }, [dispatch]);
 
   console.log(data);
 
-  return <div>Homeasdf assdhdfdda</div>;
+ // if (isLoading){
+
+ // }
+
+  //if (error){
+//    return <Error message={error} />
+//}
+
+  return <div>Homes {data[0].name} </div>;
 }
 
 export default Home;
--- src/redux/charactersSlice/charactersSlice.js
+++ src/redux/charactersSlice/charactersSlice.js
@@ -2,8 +2,9 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchCharacters = createAsyncThunk(
-  "characters/getCharacters", async () => {
-    const res = await axios("https://jsonplaceholder.typicode.com/users");
+  "characters/getCharacters",
+  async () => {
+    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10`);
     return res.data;
   }
 );
@@ -12,14 +13,22 @@ export const charactersSlice = createSlice({
   name: "characters",
   initialState: {
     items: [],
+    isLoading: false,
   },
   reducers: {},
   extraReducers: {
+    //[fetchCharacters.pending]: (state, action) => {
+    //  state.isLoading = true;
+   // },
     [fetchCharacters.fulfilled]: (state, action) => {
-        console.log(action.payload)
-    }
+      state.items = action.payload;
+      //state.isLoading = false;
+    },
+    //[fetchCharacters.fulfilled]: (state, action) => {
+    //  state.isLoading = false;
+    //  state.error = action.error.message;
+   // },
   },
 });
 
 export default charactersSlice.reducer;
-
