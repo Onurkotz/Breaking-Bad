36c3906e875ed304eda7bac42b1ff227822006e4
onur koc <onurkottz@gmail.com>
2022-04-06 13:34:15 +0300

    masonry grid system

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,51 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCharacters } from "../../redux/charactersSlice/charactersSlice";
-//import Loading from "../Loading/Loading";
-//import Error from "../Error/Error";
+import Masonry from "react-masonry-css";
+import "./style.css";
+import Loading from "../Loading/Loading";
+import Error from "../Error/Error";
 
 function Home() {
   const data = useSelector((state) => state.characters.items);
-  //const isLoading = useSelector((state) => state.characters.isLoading);
- // const error = useSelector((state) => state.characters.error);
+  const isLoading = useSelector((state) => state.characters.isLoading);
+  const error = useSelector((state) => state.characters.error);
   const dispatch = useDispatch();
-  
-  
+
   useEffect(() => {
     dispatch(fetchCharacters());
   }, [dispatch]);
 
   console.log(data);
 
- // if (isLoading){
+  if (isLoading) {
+    return <Loading />;
+  }
 
- // }
+  if (error) {
+    return <Error message={error} />;
+  }
 
-  //if (error){
-//    return <Error message={error} />
-//}
-
-  return <div>Homes {data[0].name} </div>;
+  return (
+    <div>
+      <Masonry
+        breakpointCols={4}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {data.map((character) => (
+          <div key={character.char_id}>
+            <img
+              alt={character.name}
+              src={character.img}
+              className="character"
+            />
+            <h4>{character.name}</h4>
+          </div>
+        ))}
+      </Masonry>
+    </div>
+  );
 }
 
 export default Home;
--- src/redux/charactersSlice/charactersSlice.js
+++ src/redux/charactersSlice/charactersSlice.js
@@ -1,10 +1,14 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const char_limit = 12;
+
 export const fetchCharacters = createAsyncThunk(
   "characters/getCharacters",
   async () => {
-    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=10`);
+    const res = await axios(
+      `${process.env.REACT_APP_API_BASE_ENDPOINT}/characters?limit=${char_limit}`
+    );
     return res.data;
   }
 );
